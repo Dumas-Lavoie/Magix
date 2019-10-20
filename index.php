233992e406ecaa81ad32517f222d47cdc594af2b
Dumas-Lavoie <1422611@decinfo.cvm>
2019-10-20 15:36:46 -0400

    Ajouts de la structure orienté objet de la partie php du projet

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Magix</title>
-</head>
-<body style="display: flex; flex-direction: column;">
+<?php
+	require_once("action/LoginAction.php");
+
+	$action = new LoginAction();
+	$action->execute();
+
+	require_once("partial/header.php");
+?>
 	<h1>Login</h1>
 	<form>
-		<input type="text" placeholder="Username"></input>
-		<input type="password" placeholder="Password"></input>
+		<input type="text" placeholder="Username">
+		<input type="password" placeholder="Password">
 		<button type="submit">Se connecter!</button>
 	</form>
 </body>
