6d335d8ca48735e4da8119a1f333a42e11fc1ed6
Félix Dumas-Lavoie <goglu1@live.ca>
2019-10-26 14:52:03 -0400

    Restrcturation du projet pour avoir plusieurs landing pages

--- lobby.php
+++ lobby.php
@@ -7,7 +7,25 @@
 	require_once("partial/header.php");
 ?>
 
+<?php
+			if (isset($_GET["deconnexion"]))
+			{
+				$action->logout();
+			}
+			if ($action->estConnecte) {
+				?>
+				<script></script>
+				<div id="connecte">
+				<div class="error-div"><strong>Vous êtes connecté : </strong>Connexion réussie</div>
+				<form action="index.php" method="get">
+				<button name="deconnexion" type="submit">Se déconnecter!</button>
+				</div>
+				</form>
 
+			<?php
+			}
+
+			?>
 
 </body>
 </html>
